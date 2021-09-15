66120acf7cee8d39caedd24ab4f7d9848d50710c
Jean <jeanramalho.dev@gmail.com>
2021-09-15 12:03:24 -0300

    implementa modal com informações do filme

--- views/search/index.js
+++ views/search/index.js
@@ -2,6 +2,8 @@ let titulo = document.getElementById('search')
 let results = document.getElementById('results')
 let imgResultado = document.getElementById('imgResultado')
 let info = document.getElementById('info')
+let erro = document.getElementById('erro')
+let movieName = document.getElementById('movieName')
 
 $('#btn-search').on('click', ()=>{
     let url =  `http://www.omdbapi.com/?apikey=42d7f882&t=${titulo.value}`
@@ -13,9 +15,19 @@ $('#btn-search').on('click', ()=>{
         dataType: "json",
 
         success: function(dados) {
-            imgResultado.setAttribute('src', `${dados.Poster}`)
-            info.innerHTML = `Title : ${dados.Title} </br> Year: ${dados.Year} </br> Plot: ${dados.Plot} </br> Actors: ${dados.Actors} `
-            console.log(dados.Poster)
-        }
-    })
-})
+                if(dados.Response == 'False') {
+                    imgResultado.src = ""
+                    info.innerHTML = ""
+                    erro.innerHTML = "<h1>Movie not found!</h1>"
+                }
+                else {  
+                        erro.innerHTML = ""
+                        imgResultado.setAttribute('src', `${dados.Poster}`)
+                        movieName.innerHTML = `${dados.Title}`
+                        info.innerHTML = `<strong>Title:</strong> ${dados.Title} </br> <strong>Year:</strong> ${dados.Year} </br> <strong>Plot:</strong> ${dados.Plot} </br> <strong>Actors:</strong> ${dados.Actors} `
+                        console.log(dados.Poster)
+
+                    }            
+                }
+            })
+        })
