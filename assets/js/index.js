091548676ce42027896605f03bfa8fd908a9969e
William Rodrigues <williamrodrigues2442@gmail.com>
2021-09-16 07:34:12 -0300

    Adiciona carrosséis dos filmes na home

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,35 @@
-// JS da página home.
+$(() => {
+  // Ativa todos os tooltips dos carrosséis.
+  $('[data-bs-toggle="tooltip"]').tooltip();
+});
+
+// Ativa todos os carrosséis.
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 15,
+  dots: false,
+  nav: true,
+  navText: [
+    '<span class="btn btn-outline-light bi bi-arrow-left-square-fill"></span>',
+    '<span class="btn btn-outline-light bi bi-arrow-right-square-fill"></span>'
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      stagePadding: 40,
+    },
+    480: {
+      items: 2,
+      stagePadding: 20,
+    },
+    768: {
+      items: 3,
+    },
+    992: {
+      items: 4,
+    },
+    1200: {
+      items: 5,
+    }
+  },
+});
